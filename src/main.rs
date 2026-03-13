a261a71a82a9d9a087ce6247491ab4264b074ace
NWise-DrownedOctopus <nicholasmwise@gmail.com>
2026-03-13 12:54:51 -0500

    Added in Basic Board

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,39 @@ slint::include_modules!();
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
+    let mut board: [bool; 49] = [
+        false, false, true,  true,  true,  false, false,
+        false, false, true,  true,  true,  false, false,
+        true,  true,  true,  true,  true,  true,  true,
+        true,  true,  true,  false, true,  true,  true,
+        true,  true,  true,  true,  true,  true,  true,
+        false, false, true,  true,  true,  false, false,
+        false, false, true,  true,  true,  false, false,
+    ];
 
-    ui.on_request_decrease_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() - 1);
-        }
-    });
+    // Store selected cell
+    let selected_start = std::cell::RefCell::new(None::<usize>);
+
+    // ui.on_try_move(move |name| {
+    //     let index: usize = name.strip_prefix("cell-").unwrap().parse().unwrap();
+
+    //     if let Some(start_index) = selected_start.borrow_mut().take() {
+    //         // Try to perform move from start_index -> index
+    //         let (sr, sc) = (start_index / 7, start_index % 7);
+    //         let (er, ec) = (index / 7, index % 7);
+
+    //         if is_valid_move(sr, sc, er, ec, &board) {
+    //             execute_move(sr, sc, er, ec, &mut board);
+    //             app.set_board_state(board.to_vec());
+    //         } else {
+    //             println!("Invalid move from {} to {}", start_index, index);
+    //         }
+    //     } else {
+    //         // First click
+    //         *selected_start.borrow_mut() = Some(index);
+    //         app.set_selected_start(name.into());
+    //     }
+    // });
 
     ui.run()?;
 
